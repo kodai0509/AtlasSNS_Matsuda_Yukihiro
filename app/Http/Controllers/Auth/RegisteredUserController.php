a80b097cdf8e5907d66e45dd371afe07b25d666c
KOH\matsu <matsuda0509yukihiro@gmail.com>
2024-09-17 17:29:40 +0900

    バリデーション処理

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'password' =>['required','string','min:8','max:20','regex:/^[a-zA-Z0-9]+$/'],
             'password_confirmation' =>['required','string','min:8','max:20','same:password','regex:/^[a-zA-Z0-9]+$/'],
         ]);
+
         User::create([
             'username' => $request->username,
             'email' => $request->email,
