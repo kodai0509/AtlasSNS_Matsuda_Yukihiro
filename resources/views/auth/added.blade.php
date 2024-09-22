df4d6b8ff78f45fd8268a88f6c8356665356230f
KOH\matsu <matsuda0509yukihiro@gmail.com>
2024-09-22 22:12:07 +0900

    ログイン後に表示するページにアクセス制限をかける

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -1,10 +1,17 @@
 <x-logout-layout>
   <div id="clear">
-    <p>〇〇さん</p>
+    <p>{{ auth()->user()->name }}さん</p>
     <p>ようこそ！AtlasSNSへ！</p>
     <p>ユーザー登録が完了しました。</p>
     <p>早速ログインをしてみましょう。</p>
 
-    <p class="btn"><a href="login">ログイン画面へ</a></p>
+    <p class="btn"><a href="{{ route('login') }}">ログイン画面へ</a></p>
   </div>
+
+  <!--未認証の場合はログイン画面へ -->
+    @endauth
+    @guest
+    <p>ログインしてください。</p>
+    @endguest
+
 </x-logout-layout>
