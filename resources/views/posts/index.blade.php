e1143bcbbdf58b845472e3cbc1e8328d9e2760da
KOH\matsu <matsuda0509yukihiro@gmail.com>
2024-10-12 21:50:18 +0900

    トップページ機能実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,105 +1,55 @@
 <x-login-layout>
-   <div class="container">
-      {!! Form::open(['url' => '/posts/index']) !!}
+    <!-- 新しい投稿フォーム -->
+    <div class='container'>
+        {!! Form::open(['route' => 'posts.store']) !!}
+        <div class="form-group">
+            <img src="{{ asset('images/icon1.png') }}">
+            {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください。']) !!}
+            <button type="submit" class="btn pull-right">
+                <img src="{{ asset('images/post.png') }}">
+            </button>
+        </div>
+        {!! Form::close() !!}
+    </div>
 
-      <!-- 新しい投稿フォーム -->
-      <div class='post'>
-         <img src="{{ asset('images/icon1.png') }}">
-         {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください。']) !!}
-         <div class="button">
-            <button type="submit"><img src="{{ asset('images/post.png') }}"></button>
-         </div>
-      </div>
-      {!! Form::close() !!}
+    <!-- メッセージ表示 -->
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+    @if(session('error'))
+        <div class="alert alert-danger">{{ session('error') }}</div>
+    @endif
 
-      <!-- 投稿を表示 -->
-      @foreach($posts as $post)
-         <div class="content">
-             @if($post->user)
-
-            <p>{{ $post->user->username }}</p>
-            <p>{{ $post->post }}</p>
-
-            <!-- 投稿の編集ボタン -->
-            @if(auth()->user()->id === $post->user_id)
-               <a class="js-modal-open" href="javascript:void(0);" data-post="{{ $post->post }}" data-post-id="{{ $post->id }}">
-                  <img src="{{ asset('images/edit.png') }}">
-               </a>
-            @endif
-
-            @else
-                <p>この投稿のユーザー情報は存在しません。</p>
-            @endif
-         </div>
-      @endforeach
-
-      @if($posts->isEmpty())
-          <p>投稿がありません。</p>
-      @endif
-
-      <!-- モーダルの中身 -->
-      <div class="modal js-modal">
-         <div class="modal__bg js-modal-close"></div>
-         <div class="modal__content">
-            <!-- 編集フォーム -->
-            {!! Form::open(['method' => 'put', 'id' => 'editForm']) !!}
-               <textarea name="post" class="modal_post" maxlength="150"></textarea>
-               <input type="hidden" name="post_id" class="modal_id" value="">
-               <input type="submit" value="更新">
-               {{ csrf_field() }}
-            {!! Form::close() !!}
-            <a class="js-modal-close" href="javascript:void(0);">閉じる</a>
-            <!-- 削除 -->
-
-         </div>
-      </div>
-   </div>
-
-   <!-- JavaScript -->
-   <script>
-      document.addEventListener('DOMContentLoaded', function () {
-         // モーダルを開く
-         const modalOpenLinks = document.querySelectorAll('.js-modal-open');
-         const modal = document.querySelector('.js-modal');
-         const modalCloseLinks = document.querySelectorAll('.js-modal-close');
-         const modalPostInput = document.querySelector('.modal_post');
-         const modalIdInput = document.querySelector('.modal_id');
-         const editForm = document.getElementById('editForm');
-
-         modalOpenLinks.forEach(link => {
-            link.addEventListener('click', function (e) {
-               e.preventDefault();
-               const post = this.getAttribute('data-post');
-               const postId = this.getAttribute('data-post-id');
-
-               // モーダルにデータを設定
-               modalPostInput.value = post;
-               modalIdInput.value = postId;
-
-               // フォームのアクションURLを設定
-               editForm.action = `/posts/update/${postId}`;
-
-               // モーダルを表示
-               modal.style.display = 'block';
-            });
-         });
-
-         // モーダルを閉じる
-         modalCloseLinks.forEach(link => {
-            link.addEventListener('click', function (e) {
-               e.preventDefault();
-               modal.style.display = 'none';
-            });
-         });
-
-         // 背景クリックでモーダルを閉じる
-         document.querySelector('.modal__bg').addEventListener('click', function () {
-            modal.style.display = 'none';
-         });
-      });
-   </script>
-
-   <!-- Bootstrap用スクリプト -->
-   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
+    <!-- 投稿を表示 -->
+    <table class="table table-hover">
+        @foreach($posts as $post)
+            <tr>
+                <th><img src="{{ asset('images/icon1.png') }}"></th>
+                <th>{{ $post->user->username }}</th>
+                <th>{{ $post->post }}</th>
+                <th>{{ $post->created_at }}</th>
+                <!-- 編集フォーム -->
+                <th>
+                    <form method="POST" action="{{ route('posts.update', $post->id) }}" accept-charset="UTF-8" id="editForm">
+                        @csrf
+                        @method('PUT')
+                        <textarea name="post" class="modal_post" maxlength="150">{{ $post->post }}</textarea>
+                        <button type="submit" class="btn btn-primary">
+                            <img src="{{ asset('images/edit.png') }}">
+                        </button>
+                    </form>
+                </th>
+                <!-- 削除 -->
+                 <th>
+                    <form method="POST" action="{{ route('posts.destroy', $post->id) }}" onsubmit="return confirm('こちらの投稿を削除してもよろしいでしょうか？');">
+                    @csrf
+                    @method('DELETE')
+                      <button type="submit" class="btn btn-danger">
+                        <img src="{{ asset('images/trash-h.png') }}">
+                       </button>
+                     </form>
+                 </th>
+            </tr>
+        @endforeach
+    </table>
 </x-login-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,39 +8,36 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 require __DIR__ . '/auth.php';
 
 // ミドルウェア設定//
 Route::middleware('auth')->group(function () {
 
-//トップページ
-Route::get('/index', [RegisteredUserController::class, 'index'])->name('index');
+// トップページのルート
+    Route::get('/index', [PostsController::class, 'index'])->name('posts.index');
+
+    // 投稿の保存
+    Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+
+    // 編集
+    Route::put('/posts/update/{id}', [PostsController::class, 'update'])->name('posts.update');
+
+    // 削除
+    Route::delete('/posts/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
+
 
 // プロフィール画面
 Route::get('profile',[ProfileController::class, 'profile'])->name('profiles.profile');
 
 // 検索画面
-Route::get('search',[UsersController::class, 'index']);
+Route::get('search',[UsersController::class, 'search'])->name('search');
 
 // フォロー・フォロワーリストページ
-Route::get('follow-list',[PostsController::class, 'followList']);
-Route::get('follower-list', [PostsController::class, 'followerList']);
+Route::get('follow-list',[FollowsController::class, 'followList'])->name('follow.list');
+Route::get('follower-list', [FollowsController::class, 'followerList'])->name('follower.list');
 
 // 相手ユーザーのプロフィールページ
 Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
-
 });
 
 Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
