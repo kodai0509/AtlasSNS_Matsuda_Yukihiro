8d3338435df7549c9d30be8b9f35cd32bdd6c389
KOH\matsu <matsuda0509yukihiro@gmail.com>
2024-12-13 18:38:06 +0900

    ユーザー検索レイアウト

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,65 +1,53 @@
 <x-login-layout>
     @section('content')
     <!-- 検索フォーム -->
-    <form action="{{ route('search') }}" method="POST" class="mb-4">
-        @csrf
-        <input
-            type="text" name="keyword" class="form" placeholder="ユーザー名を入力"
-            value="{{ request('keyword') }}">
-        <button type="submit" class="btn btn-success">
-            <img src="{{ asset('images/search.png') }}" alt="Search Icon" style="width: 20px; height: 20px;">
-        </button>
-    </form>
-
-    <!-- 検索ワードの表示 -->
-    @if(!empty(request('keyword')))
-        <p>検索ワード: <strong>{{ request('keyword') }}</strong></p>
-    @endif
-
-    <!-- 検索結果の表示 -->
-    @if($users->isEmpty())
-        <p>No users found.</p>
-    @else
-
-    @foreach ($users as $user)
-    <ul style="list-style: none; padding: 0;">
-        <li style="display: flex; align-items: center; justify-content: space-between;">
-            <div style="display: flex; align-items: center;">
-                <a href="{{ route('profile.show', ['user' => $user->id]) }}">
-                    <img src="{{ asset('images/' . $user->icon_image) }}" style="width: 50px; height: 50px; margin-right: 10px;">
-                </a>
-                <p class="mb-0">{{ $user->username }}</p>
-            </div>
-            <div>
-                @if (auth()->user()->isFollowing($user->id))
-                <form action="{{ route('unfollow', ['user' => $user->id]) }}" method="POST" style="display: inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">
-                        フォロー解除
-                    </button>
-                </form>
-                @else
-                <form action="{{ route('follow', ['user' => $user->id]) }}" method="POST" style="display: inline;">
-                    @csrf
-                    <button type="submit" class="btn btn-primary">
-                        フォローする
-                    </button>
-                </form>
-                @endif
-            </div>
-        </li>
-    </ul>
-    @endforeach
-
-    @endif
-
-    <!-- リロード用ボタン -->
-    <div class="mt-4">
-        <form action="{{ route('search') }}" method="GET">
-            <!-- <button type="submit" class="btn btn-info">
-                ユーザー検索ページに戻る
-            </button> -->
+    <div class="search-form">
+        <form action="{{ route('search') }}" method="POST" class="mb-4">
+            @csrf
+            <input type="text" name="keyword" class="form" placeholder="ユーザー名を入力"
+                value="{{ request('keyword') }}">
+            <button type="submit" class="search-icon">
+                <img src="{{ asset('images/search.png') }}">
+            </button>
         </form>
+        <!-- 検索ワードの表示 -->
+        @if(!empty(request('keyword')))
+        <div class="search-result">
+            <p>検索ワード: <strong>{{ request('keyword') }}</strong></p>
+        </div>
+        @endif
+    </div>
+
+    <div class="user-index">
+        @foreach ($users as $user)
+        <ul>
+            <li class="user-item">
+                <div class="user-info">
+                    <a href="{{ route('profile.show', ['user' => $user->id]) }}">
+                        <img src="{{ asset('images/' . $user->icon_image) }}" class="user-icon">
+                    </a>
+                    <p class="user-name">{{ $user->username }}</p>
+                </div>
+                <div class="follow-btn">
+                    @if (auth()->user()->isFollowing($user->id))
+                    <form action="{{ route('unfollow', ['user' => $user->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">
+                            フォロー解除
+                        </button>
+                    </form>
+                    @else
+                    <form action="{{ route('follow', ['user' => $user->id]) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-primary">
+                            フォローする
+                        </button>
+                    </form>
+                    @endif
+                </div>
+            </li>
+        </ul>
+        @endforeach
     </div>
 </x-login-layout>
